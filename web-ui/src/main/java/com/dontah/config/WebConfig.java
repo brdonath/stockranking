ccc929a11131a0655edbee94d75e119678158da3
Bruno Donath <bruno.internet@gmail.com>
2015-10-15 22:09:34 -0300

    add gzip

--- web-ui/src/main/java/com/dontah/config/WebConfig.java
+++ web-ui/src/main/java/com/dontah/config/WebConfig.java
@@ -1,22 +1,15 @@
 package com.dontah.config;
 
+import com.planetj.servlet.filter.compression.CompressingFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.velocity.VelocityAutoConfiguration;
-import org.springframework.context.annotation.*;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.UrlBasedViewResolver;
-import org.springframework.web.servlet.view.velocity.VelocityConfig;
-import org.springframework.web.servlet.view.velocity.VelocityConfigurer;
-import org.springframework.web.servlet.view.velocity.VelocityView;
-import org.springframework.web.servlet.view.velocity.VelocityViewResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
-import java.util.Properties;
+import javax.servlet.Filter;
 
 /**
  * Created by Bruno on 24/07/14.
@@ -26,11 +19,17 @@ import java.util.Properties;
         exclude = {
                 HibernateJpaAutoConfiguration.class})
 @ComponentScan("com.dontah")
-@PropertySource({"application.properties","db.properties"})
+@PropertySource({"application.properties", "db.properties"})
 public class WebConfig {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(WebConfig.class, args);
+    }
+
+    @Bean
+    public Filter compressingFilter() {
+        CompressingFilter compressingFilter = new CompressingFilter();
+        return compressingFilter;
     }
 
 }
