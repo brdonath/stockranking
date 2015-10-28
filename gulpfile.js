f2493c10b7a05fc110d8ee90b613ba6a73c709a6
Nelma <nelmasb@gmail.com>
2015-10-28 21:15:55 -0200

    removi jquery e boostrap - rodar npm install para baixar node_modules e depois rodar node_modules/gulp/bin/gulp.js default

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var bower =require('gulp-bower');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var minify = require('gulp-minify');
+
+gulp.task('bower', function() {
+   return bower();
+});
+
+gulp.task('minify-css', function() {
+   return gulp.src('web-ui/src/main/resources/static/css/*.css')
+       .pipe(minifyCss())
+       .pipe(rename({suffix: '.min'}))
+       .pipe(gulp.dest('web-ui/src/main/resources/static/css/'));
+});
+
+gulp.task('compress', function() {
+    gulp.src('web-ui/src/main/resources/static/js/*.js')
+        .pipe(minify({
+            ignoreFiles: ['*-min.js']
+        }))
+        .pipe(gulp.dest('web-ui/src/main/resources/static/js/'))
+});
+
+gulp.task('default', ['bower','minify-css','compress']);
